158ca7d438f9cc6f7c0dbd563269702be65bafff
dk2547 <dk2547@gmail.com>
2023-12-26 01:21:44 +0530

    initial commit

---
+++ src/main/java/com/example/gcpjenkins/GcpJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.gcpjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gcpjenkins/GcpJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gcpjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
